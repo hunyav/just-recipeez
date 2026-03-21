fb0973116465cc15548446c34306577f6c400c37
hunyav <hunteryavitz@protonmail.com>
2026-03-21 02:11:03 +0000

    Restore root scaffold insets so list content position actually shifts

--- app/src/main/java/com/example/justrecipeez/navigation/RecipeNavGraph.kt
+++ app/src/main/java/com/example/justrecipeez/navigation/RecipeNavGraph.kt
@@ -1,6 +1,5 @@
 package com.example.justrecipeez.navigation
 
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MenuBook
@@ -39,7 +38,6 @@ fun RecipeNavGraph(
     val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
 
     Scaffold(
-        contentWindowInsets = WindowInsets(0, 0, 0, 0),
         bottomBar = {
             NavigationBar {
                 NavigationBarItem(
